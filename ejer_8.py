98b54c057d76123ccb1f4a6aa9d1c43dd59b410c
matiasmf89 <mancuellomatias@gmail.com>
2018-06-19 19:36:30 -0300

    elimino encender linterna

--- ejer_8.py
+++ ejer_8.py
@@ -29,10 +29,10 @@ class Celular(Telefono,Camara,ReproductorMp3):
 	def ver_credito(self):
 		print('Crédito: ',self.__credito)
 		
-	def encender_linterna(self):
-		print('linterna prendida')
 
 	def apagar_linterna(self):
+		print('linterna apagada')
+		print('linterna apagada')
 		print('linterna apagada')
 			
 celular = Celular('SAMSUNG','S10','NEGRO',303456)
